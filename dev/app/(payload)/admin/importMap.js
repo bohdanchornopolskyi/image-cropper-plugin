ec2d5cf6410b9cbef006131a783bbf06b12e6a1f
Bohdan Chornopolskyi <bohdan.chornopolskyi@gmail.com>
2026-03-31 17:55:43 +0200

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'image-cropper-plugin/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'image-cropper-plugin/rsc'
+
+export const importMap = {
+  'image-cropper-plugin/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'image-cropper-plugin/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
